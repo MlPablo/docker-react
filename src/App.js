e872325a76e6162223bb088f5a76e4aff006859e
Pavlo Malov <malovanniy@gmail.com>
2022-09-05 15:22:45 +0300

    add change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello aa!
+          I was deployed
         </p>
         <a
           className="App-link"
